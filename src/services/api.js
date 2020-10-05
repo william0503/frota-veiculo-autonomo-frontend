4a9799625713a50986f7352c66606e1938ac6ddb
William Rocha <william0503@gmail.com>
2020-10-05 19:31:41 -0300

    Adiciona amplify para publish

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-    //baseURL:'http://localhost:3001/api/'
-    baseURL:`${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}/api/`
-})
+  //baseURL:'http://localhost:3001/api/'
+  baseURL: `${process.env.REACT_APP_SERVER_HOST}/api/`,
+});
 
 export default api;
